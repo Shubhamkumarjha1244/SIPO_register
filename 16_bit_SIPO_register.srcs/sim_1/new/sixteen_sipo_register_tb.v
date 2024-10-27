60a6a625d37debb8d571215276e295d42d229e82
Shubhamkumarjha1244 <jha.kumar.shubham1244@gmail.com>
2024-10-27 11:16:00 +0530

    second commit--working

--- 16_bit_SIPO_register.srcs/sim_1/new/sixteen_sipo_register_tb.v
+++ 16_bit_SIPO_register.srcs/sim_1/new/sixteen_sipo_register_tb.v
@@ -22,29 +22,31 @@
 
 module sixteen_sipo_register_tb();
     reg clk=0,in_out;
-    reg inn;
+    reg inn=1;
     wire[0:15] out;
     
-    sixteen_bit_SIPO_register dut(clk,in_out,inn,out[0],out[1],out[2],out[3],out[4],out[5],out[6],out[7],out[8],out[9],out[10],out[11],
-out[12],out[13],out[14],out[15]);
+    sixteen_bit_SIPO_register dut(clk,in_out,inn,out[0],out[1],out[2],out[3],out[4],out[5],out[6],out[7],out[8],
+    out[9],out[10],out[11],out[12],out[13],out[14],out[15]);
+
+    always
+    #5 clk=~clk;
+
+    initial 
+        in_out=0;
+
 
 always @(posedge clk)
-#5 clk=~clk;
-
-initial 
-    in_out=0;
-
-
-
-always @(posedge clk)
-    inn=1;
+    inn=~inn;
     
-initial
-    #157 in_out=1;
+    
+    initial
+        #170 in_out=1;
+      
         
         
-initial
-#200 $finish;
+        
+    initial
+    #200 $finish;
         
         
 
--- 16_bit_SIPO_register.srcs/sources_1/new/sixteenbit_SIPO_register.v
+++ 16_bit_SIPO_register.srcs/sources_1/new/sixteenbit_SIPO_register.v
@@ -23,7 +23,7 @@
 module sixteen_bit_SIPO_register(clk,in_out,inn,out0,out1,out2,out3,out4,out5,out6,out7,out8,out9,out10,out11,
 out12,out13,out14,out15);
 
-input inn,clk,in_out; //in_out kab output aayega aur kab jayega ye control karta 0=in&1=out
+input clk,inn,in_out;                                   //in_out kab output aayega aur kab jayega ye control karta 0=in&1=out
 output reg out0,out1,out2,out3,out4,out5,out6,out7,out8,out9,out10,out11,
 out12,out13,out14,out15;
 
@@ -33,33 +33,26 @@ reg[15:0] mem ;
 always @(posedge clk)
     if(~in_out)
         begin
-        mem[0]<= inn;
-        mem<= mem>>1;
+        mem=mem>>1;
+        mem[15]=inn;
         end
     else
         begin
-        out0<=mem[0];
-        out1<=mem[1];
-        out2<=mem[2];
-        out3<=mem[3];
-        out4<=mem[4];
-        out5<=mem[5];
-        out6<=mem[6];
-        out7<=mem[7];
-        out8<=mem[8];
-        out9<=mem[9];
-        out10<=mem[10];
-        out11<=mem[11];
-        out12<=mem[12];
-        out13<=mem[13];
-        out14<=mem[14];
-        out15<=mem[15];
-        end
-        
-        
-        
-        
-
-
-
+            out0=mem[0];
+            out1=mem[1];
+            out2=mem[2];
+            out3=mem[3];
+            out4=mem[4];
+            out5=mem[5];
+            out6=mem[6];
+            out7=mem[7];
+            out8=mem[8];
+            out9=mem[9];
+            out10=mem[10];
+            out11=mem[11];
+            out12=mem[12];
+            out13=mem[13];
+            out14=mem[14];
+            out15=mem[15];
+        end       
 endmodule
